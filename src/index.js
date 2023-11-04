b2ea9fc5af2c52f1c1829b965b3de87e65ddcd3a
deb1265 <117177026+deb1265@users.noreply.github.com>
2023-11-04 11:12:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.css';   // Importing CSS for global styles
+import App from './App'; // Import the main App component
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +11,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Other scripts like service workers or web vitals may follow...
+
