d88c6a75ea90244dd65d4b743c2710febd1e6139
deb1265 <117177026+deb1265@users.noreply.github.com>
2023-11-06 08:57:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [maincodeOutput, setMaincodeOutput] = useState('');
+
+  useEffect(() => {
+    // Fetch the output when the component mounts
+    fetch('/run-maincode')
+      .then(response => response.text())
+      .then(output => {
+        setMaincodeOutput(output);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>Maincode.js Output:</h1>
+      <pre>{maincodeOutput}</pre>
     </div>
   );
 }
 
 export default App;
+
